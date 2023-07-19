b2751c6ba9e9359b3e4202f48b79981464398679
AbdallahAli510 <aaliabdelhameed1@gmail.com>
2023-07-18 17:20:19 -0700

    task 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
-* print_alphabet_x10 - Prints the alphabet in lowercase 10 times using the _putchar function
+*print_alphabet_x10 -Prints the alphabetin lowercase 10times using the _putchar function
  */
 void print_alphabet_x10(void)
 {
