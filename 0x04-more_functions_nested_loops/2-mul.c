a3b10efffa5a2cf51309bb973349ddb8bfa79733
AbdallahAli510 <aaliabdelhameed1@gmail.com>
2023-07-20 18:31:15 -0700

    task 2&3&4&5&6&7&8&9&10

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+*mul-checksforchecks foradigit(0through9).
+*@a:a-Variable
+*@b:b-variable
+*Return:Always0.
+*/
+int mul(int a, int b)
+{
+int mul;
+mul = a * b;
+return (mul);
+}
+
---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+*print_numbers-checksforchecksforadigit(0through9).
+*Return:Always0.
+*/
+void print_numbers(void)
+{
+int c;
+for (c = 48; c < 58; c++)
+{
+_putchar(c);
+}
+_putchar('\n');
+}
+
