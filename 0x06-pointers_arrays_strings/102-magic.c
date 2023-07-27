e9eca2a0718ea2bbe3ce52f9244c458fdba7bd62
AbdallahAli510 <aaliabdelhameed1@gmail.com>
2023-07-27 06:23:49 -0700

    task 101

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -7,8 +7,8 @@ int *p;
 a[2] = 1024;
 p = &n;
 /*
-* The added line of code below uses pointer arithmetic to access a memory location
-* five integers ahead of the address pointed by 'p' and assigns the value 98 to that location.
+*Theaddedlineofcodebelowusespointerarithmetictoaccessamemorylocation
+*fiveintegersaheadoftheaddresspointedby'p'andassignsthe value 98 tothatlocation.
 */
 *(p + 5) = 98;
 /* ...so that this prints 98\n */
